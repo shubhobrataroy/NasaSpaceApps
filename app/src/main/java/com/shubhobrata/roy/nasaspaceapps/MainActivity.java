58c1c8f6afa53f451ac254b8812c3debe1e8bac1
shubh <shubho16.2@gmail.com>
2017-04-28 12:24:22 +0600

    Secondary push

--- app/src/main/java/com/shubhobrata/roy/nasaspaceapps/MainActivity.java
+++ app/src/main/java/com/shubhobrata/roy/nasaspaceapps/MainActivity.java
@@ -1,7 +1,10 @@
 package com.shubhobrata.roy.nasaspaceapps;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initialize
+        view_landslide_on_map= (Button) findViewById(R.id.view_data_on_map);
+
+
+        //Listeners
+        view_landslide_on_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,LandslideViewerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+    //Button
+    Button view_landslide_on_map ;
 }
